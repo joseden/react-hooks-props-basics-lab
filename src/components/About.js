9b540d990105c0421430545cacb7fe365441440a
Joseph <joseph.dena@student.moringaschool.com>
2023-11-23 11:22:50 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+// Define props as a parameter
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio && <p>{props.bio}</p>}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="" />
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
